bf43bfff36938d8731ac3d2f53a192d771d6d403
Fernando Cardoso Garcia Filho <fernando061312@gmail.com>
2016-10-04 18:18:04 -0300

    Adicionado WebMail

---
+++ PetShopJS/Controllers/ContatoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace PetShopJS.Controllers {
+    public class ContatoController : Controller {
+        // GET: Index view  
+        public ActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail() {
+            //call SendEmailView view to invoke webmail  
+            return View();
+        }
+    }
+}
---
+++ PetShopJS/Models/ContatoModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetShopJS.Models {
+    public class ContatoModel {
+        [DataType(DataType.EmailAddress), Display(Name = "Para")]
+        [Required]
+        public string ToEmail { get; set; }
+        [Display(Name = "Mensagem")]
+        [DataType(DataType.MultilineText)]
+        public string EMailBody { get; set; }
+        [Display(Name = "Assunto")]
+        public string EmailSubject { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "CC")]
+        public string EmailCC { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "CCO")]
+        public string EmailBCC { get; set; }
+    }
+}
